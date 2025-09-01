17ca98c09735b1f6760aef339b9e1e48103109ee
Nikita Shestakov <takasy94@gmail.com>
2025-09-01 14:09:24 +0300

    доработан прокси сервис

--- src/microservices/proxy/src/main/java/org/example/ProxyController.java
+++ src/microservices/proxy/src/main/java/org/example/ProxyController.java
@@ -6,8 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
@@ -36,10 +35,13 @@ public class ProxyController {
     }
 
     @GetMapping("/api/movies")
-    public ResponseEntity<String> proxyMoviesRequest() {
+    public ResponseEntity<String> proxyGetMovieById(@RequestParam(required = false) String id) {
         String targetUrl = determineTargetUrl();
         String fullUrl = targetUrl + "/api/movies";
-        log.info("Proxying /api/movies request to {}", fullUrl);
+        if (id != null) {
+            fullUrl = targetUrl + "/api/movies?id=" + id;
+            log.info("Proxying /api/movies?id={} request to {}", id, fullUrl);
+        } else log.info("Proxying /api/movies request to {}", fullUrl);
         try {
             return restTemplate.getForEntity(fullUrl, String.class);
         } catch (Exception e) {
@@ -47,16 +49,93 @@ public class ProxyController {
         }
     }
 
+    @PostMapping("/api/movies")
+    public ResponseEntity<String> proxyCreateMovieRequest(@RequestBody String body) {
+        String targetUrl = determineTargetUrl();
+        String fullUrl = targetUrl + "/api/movies";
+        log.info("Proxying POST /api/movies request to {}", fullUrl);
+        try {
+            return restTemplate.postForEntity(fullUrl, body, String.class);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error proxying request: " + e.getMessage());
+        }
+    }
+
     @GetMapping("/api/users")
-    public ResponseEntity<String> proxyUsersRequest() {
+    public ResponseEntity<String> proxyGetUserById(@RequestParam(required = false) String id) {
         String fullUrl = monolithUrl + "/api/users";
-        log.info("Proxying /api/users request to {}", fullUrl);
+        if (id != null){
+            fullUrl = monolithUrl + "/api/users?id=" + id;
+            log.info("Proxying /api/users?id={} request to {}", id, fullUrl);
+        } else log.info("Proxying /api/users request to {}", fullUrl);
         try {
             return restTemplate.getForEntity(fullUrl, String.class);
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error proxying request: " + e.getMessage());
         }
     }
+
+    @PostMapping("/api/users")
+    public ResponseEntity<String> proxyCreateUserRequest(@RequestBody String body) {
+        String fullUrl = monolithUrl + "/api/users";
+        log.info("Proxying POST /api/users request to {}", fullUrl);
+        try {
+            return restTemplate.postForEntity(fullUrl, body, String.class);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error proxying request: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/api/payments")
+    public ResponseEntity<String> proxyGetPaymentById(@RequestParam(required = false) String id) {
+        String fullUrl = monolithUrl + "/api/payments";
+        if (id != null) {
+            fullUrl = monolithUrl + "/api/payments?id=" + id;
+            log.info("Proxying /api/payments?id={} request to {}", id, fullUrl);
+        } else log.info("Proxying /api/payments request to {}", fullUrl);
+        try {
+            return restTemplate.getForEntity(fullUrl, String.class);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error proxying request: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/api/payments")
+    public ResponseEntity<String> proxyCreatePaymentRequest(@RequestBody String body) {
+        String fullUrl = monolithUrl + "/api/payments";
+        log.info("Proxying POST /api/payments request to {}", fullUrl);
+        try {
+            return restTemplate.postForEntity(fullUrl, body, String.class);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error proxying request: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/api/subscriptions")
+    public ResponseEntity<String> proxyGetSubscriptionById(@RequestParam(required = false) String id) {
+        String fullUrl = monolithUrl + "/api/subscriptions";
+        if (id != null) {
+            fullUrl = monolithUrl + "/api/subscriptions?id=" + id;
+            log.info("Proxying /api/subscriptions?id={} request to {}", id, fullUrl);
+        } else log.info("Proxying /api/subscriptions request to {}", fullUrl);
+        try {
+            return restTemplate.getForEntity(fullUrl, String.class);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error proxying request: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/api/subscriptions")
+    public ResponseEntity<String> proxyCreateSubscriptionRequest(@RequestBody String body) {
+        String fullUrl = monolithUrl + "/api/subscriptions";
+        log.info("Proxying POST /api/subscriptions request to {}", fullUrl);
+        try {
+            return restTemplate.postForEntity(fullUrl, body, String.class);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error proxying request: " + e.getMessage());
+        }
+    }
+
 
     private String determineTargetUrl() {
         if (!gradualMigrationEnabled) {
